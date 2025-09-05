db1fc6edc541434b33df854889051884f0370f6e
Marina Ranenko <mranenko@gmail.com>
2025-09-05 13:54:50 -0600

    colors

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -9,6 +9,11 @@ const DashboardPage = () => {
   return (
     <Page className="dashboard-page">
       <EyebrowTitle eyebrow="Compass Design System" title="Dashboard" />
+      <p>
+        This design system was created for a learning management system (LMS) and includes two
+        color themes, each with light and dark modes. Any and all parts of this design system can
+        be changed or expanded, or used as provided here.
+      </p>
     </Page>
   )
 };
