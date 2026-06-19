266716848d1984d6397ec9843ab1f4c2a2ac1382
Marina Ranenko <mranenko@gmail.com>
2026-06-19 17:50:45 -0600

    footer links

--- src/components/screen/ScreenFooter.tsx
+++ src/components/screen/ScreenFooter.tsx
@@ -1,9 +1,22 @@
+import {
+  FigmaLogoIcon,
+  GithubLogoIcon,
+} from "@phosphor-icons/react";
+import { NavLink } from "react-router-dom";
 import "./ScreenFooter.css";
 
 const ScreenFooter = () => {
   return (
     <footer className="screen-footer">
-      Compass Design System
+      <NavLink to="/">Compass Design System</NavLink>
+      <nav className="screen-footer-nav">
+        <NavLink target="_blank" to="https://www.figma.com/design/xZrAdG2DbluIZ1u5UhtsBB/Compass-Design-System-2.0-2026">
+          <i className="icon"><FigmaLogoIcon /></i>
+        </NavLink>
+        <NavLink target="_blank" to="https://github.com/mranenko/compass-design-system">
+          <i className="icon"><GithubLogoIcon /></i>
+        </NavLink>
+      </nav>      
     </footer>
   );
 };
